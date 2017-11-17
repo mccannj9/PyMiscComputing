f01bc2f3d42e7fc460f2ba9ce8511a9b734e62a5
Jamie McCann <mccannj@protonmail.com>
2017-11-17 15:39:30 +0100

    Cleanup

--- SumFinder.py
+++ SumFinder.py
@@ -15,7 +15,7 @@ def find_sum_quadratic(value, numbers):
     print(value)
     for i in range(0, len(numbers)-1):
         x = numbers[i]
-        
+
         for j in range(i+1, len(numbers)):
             y = numbers[j]
 
